3f203680855b25031262ebc62a2a3f6ebafa9b37
Alvin Tan <alvintan626@berkeley.edu>
2018-02-03 14:53:36 -0800

    Create *walldrawing527.js

---
+++ http/massmoca.org/event/walldrawing527/*walldrawing527.js
@@ -0,0 +1,15 @@
+createCanvas(300,200);
+stroke(25);
+rect(0,0,300,200);
+
+fill(233);
+beginShape();
+vertex(0, 0);
+vertex(200, 40);
+vertex(300,0);
+vertex(300,200);
+vertex(200, 140);
+vertex(0, 200);
+vertex(0, 0);
+endShape();
+line(200,40,200,140);
