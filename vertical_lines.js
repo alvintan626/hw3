2b9631e8045dcb41bd5b3382f1034800a9d9815a
Alvin Tan <alvintan626@berkeley.edu>
2018-02-02 22:41:36 -0800

    Create vertical_lines.js

---
+++ vertical_lines.js
@@ -0,0 +1,13 @@
+function yourPatch(x, y) {
+  noFill();
+  stroke(0);
+  rect(x, y, 300, 300);
+
+  strokeWeight(2);
+for (i=10; i<300; i=i+10) {
+  line(i,0,i,300);
+}
+}
+
+background(255);
+yourPatch(0, 0); // draw patch at upper-left
